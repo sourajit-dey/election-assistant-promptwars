9abc5cb63fd39afae411ff36469c1e93db31abfa
sourajit-dey <sourajitdey261@gmail.com>
2026-04-30 13:25:24 +0530

    chore: remove config.js from git to deploy securely via Google Cloud

--- js/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file config.js
- * @description Application configuration and API key storage
- * @author VoteGuide India
- * @version 1.0.0
- */
-
-// Get your key from aistudio.google.com
-const GEMINI_API_KEY = 'AIzaSyAgePt800pOEjw7bBMrhcT2n3sXJRoTYzs';
